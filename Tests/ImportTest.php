df9706f25879a1a1c1a211b103d90b1717a7fedb
jandre1907 <julien.andre1907@gmail.com>
2014-09-01 19:43:22 +0200

    supress space in json, camelize

--- Tests/ImportTest.php
+++ Tests/ImportTest.php
@@ -28,29 +28,30 @@ class ImportTest extends WebTestCase
             'Tms\Bundle\OperationBundle\Entity\Product',
             '{
                 "name":"test",
-                "short_description": "courte description",
-                "long_description": "longue description",
-                "ean13":"whatis an ean13",
-                "customer_id": 1,
-                "refundable": 1
+                "shortDescription":"courte description",
+                "longDescription":"longue description",
+                "ean13":"1",
+                "customer":"1",
+                "refundable":"1"
             }',
             'json'
         );
+        var_dump(print_r($entity));
         $this->assertInstanceOf("Tms\Bundle\OperationBundle\Entity\Product", $entity, "not a Tms\Bundle\OperationBundle\Entity\Product");
-        $name = "test" . sha1(date(""));
-        $importService->import(
-            $em,
-            'Tms\Bundle\OperationBundle\Entity\Product',
-            '{
-                "short_description": "courte description",
-                "long_description": "longue description",
-                "ean13":"whatis an ean13",
-                "customer_id": 1,
-                "refundable": 1,
-                "name": "'.$name.'"
-            }',
-            'json'
-        );
+        // $name = "test" . sha1(date(""));
+        // $importService->import(
+        //     $em,
+        //     'Tms\Bundle\OperationBundle\Entity\Product',
+        //     '{
+        //         "customer":"1",
+        //         "refundable":"1",
+        //         "name": "'.$name.'",
+        //         "shortDescription":"courte description",
+        //         "longDescription":"longue description",
+        //         "ean13":"1"
+        //     }',
+        //     'json'
+        // );
         // $product  = $em->getRepository("Tms\Bundle\OperationBundle\Entity\Repository\ProductRepository")->findOneBy(array('name'=>$name));
         // var_dump($product);
     }
